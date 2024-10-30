094f7e7d2128317f3bd6612e8b972f37e8543438
Dhanush Govindaraju <35737348+dhanushdragneel@users.noreply.github.com>
2024-10-30 19:57:51 +0100

    Update firebaseConfig.js

--- chordle-app/src/firebaseConfig.js
+++ chordle-app/src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQTSoCa_P9nofIHOuGkS5_HvW5p5zbR9Q",
-  authDomain: "chordle-44664.firebaseapp.com",
-  projectId: "chordle-44664",
-  storageBucket: "chordle-44664.appspot.com",
-  messagingSenderId: "88668067971",
-  appId: "1:88668067971:web:7a12f57f845ad3323b23b6",
-  measurementId: "G-WJGFYVFY5G"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
